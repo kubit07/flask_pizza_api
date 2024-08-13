8bf6117fdbb6ec0ddff325ee5208db41ae54f764
Dave <davidnsd90@gmail.com>
2024-08-13 23:54:41 +0200

    Api Pizza Flask

---
+++ api/config/config.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+import os
+from decouple import config
+
+
+BASE_DIR=os.path.dirname(os.path.realpath(__file__))
+
+class Config:
+    SECRET_KEY=config('SECRET_KEY', 'secret')
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES=timedelta(minutes=30)
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    JWT_SECRET_KEY=config('JWT_SECRET_KEY')
+
+class DevConfig(Config):
+    DEBUG=config('DEBUG', cast=bool)
+    SQLALCHEMY_ECHO=True
+    SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(BASE_DIR,'db.sqlite3')
+
+class TestConfig(Config):
+    TESTING=True
+    SQLALCHEMY_DATABASE_URI="sqlite://"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_ECHO=True
+
+class ProdConfig(Config):
+    pass
+
+
+config_dict={
+    'dev':DevConfig,
+    'prod':ProdConfig,
+    'test':TestConfig
+}
---
+++ api/models/orders.py
@@ -0,0 +1,47 @@
+import datetime
+from ..utils import db
+from enum import Enum
+
+
+class Size(Enum):
+    SMALL='small'
+    MEDIUM='medium'
+    LARGE='large'
+    EXTRA_LARGE='extra-large'
+
+
+class OrderStatus(Enum):
+    PENDING='pending'
+    IN_TRANSIT='in-transit'
+    DELIVERED='delivered'
+
+
+class Order(db.Model):
+    __tablename__='orders'
+
+    id=db.Column(db.Integer(),primary_key=True)
+    size=db.Column(db.Enum(Size),default=Size.SMALL)
+    order_status=db.Column(db.Enum(OrderStatus) ,default=OrderStatus.PENDING)
+    flavour=db.Column(db.String(),nullable=False)
+    quantity=db.Column(db.Integer())
+    date_created=db.Column(db.DateTime(), default=datetime.datetime.utcnow())
+    customer=db.Column(db.Integer(),db.ForeignKey('users.id'))
+
+    def __str__(self):
+        return f"<Order {self.id}>"
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @classmethod
+    def get_by_id(cls,id):
+        return cls.query.get_or_404(id)
+
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
